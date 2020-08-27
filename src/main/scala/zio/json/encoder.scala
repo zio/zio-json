61cdab852f8ec0ce5d289864a05d41b05e56cb42
LouisLotter <40275809+LouisLotter@users.noreply.github.com>
2020-08-27 17:33:32 +0200

    set unsafe method scope to zio

--- src/main/scala/zio/json/encoder.scala
+++ src/main/scala/zio/json/encoder.scala
@@ -3,6 +3,6 @@ package zio.json
 trait Encoder[A] {
   def toJson(a: A): String
 
-  private[json] def unsafeToJson(a: A, out: java.io.Writer): Unit
-  private[json] def unsafeToJsonField(a: A, field: String, out: java.io.Writer): Unit // = ... // default impl
+  private[zio] def unsafeToJson(a: A, out: java.io.Writer): Unit
+  private[zio] def unsafeToJsonField(a: A, field: String, out: java.io.Writer): Unit // = ... // default impl
 }
