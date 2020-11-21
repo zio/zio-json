16b8a5fa61ecb6e16bfda141fc40b740af77e19a
Jason Pickens <jasonpickensnz@gmail.com>
2020-11-22 12:43:09 +1300

    Add java.time round trip tests

--- zio-json/jvm/src/test/scala/zio/json/RoundTripSpec.scala
+++ zio-json/jvm/src/test/scala/zio/json/RoundTripSpec.scala
@@ -1,7 +1,8 @@
 package testzio.json
 
-import testzio.json.TestUtils._
+import java.time._
 
+import testzio.json.TestUtils._
 import zio.json._
 import zio.json.ast.Json
 import zio.random.Random
@@ -39,7 +40,57 @@ object RoundTripSpec extends DefaultRunnableSpec {
       },
       testM("AST") {
         check(genAst)(assertRoundtrips)
-      }
+      },
+      suite("java.time")(
+        test("DayOfWeek") {
+          assertRoundtrips(DayOfWeek.MONDAY)
+        },
+        test("Duration") {
+          assertRoundtrips(Duration.ofDays(1))
+        },
+        test("Instant") {
+          assertRoundtrips(Instant.EPOCH)
+        },
+        test("LocalDate") {
+          assertRoundtrips(LocalDate.of(2020, 1, 1))
+        },
+        test("LocalDateTime") {
+          assertRoundtrips(LocalDateTime.of(2020, 1, 1, 12, 36, 0))
+        },
+        test("LocalTime") {
+          assertRoundtrips(LocalTime.of(12, 36, 0))
+        },
+        test("Month") {
+          assertRoundtrips(Month.JANUARY)
+        },
+        test("MonthDay") {
+          assertRoundtrips(MonthDay.of(1, 1))
+        },
+        test("OffsetDateTime") {
+          assertRoundtrips(OffsetDateTime.of(2020, 1, 1, 12, 36, 12, 0, ZoneOffset.UTC))
+        },
+        test("OffsetTime") {
+          assertRoundtrips(OffsetTime.of(12, 36, 12, 0, ZoneOffset.ofHours(-4)))
+        },
+        test("Period") {
+          assertRoundtrips(Period.ofDays(1))
+        },
+        test("Year") {
+          assertRoundtrips(Year.of(1999))
+        },
+        test("YearMonth") {
+          assertRoundtrips(YearMonth.of(1999, 12))
+        },
+        test("ZonedDateTime") {
+          assertRoundtrips(LocalDateTime.of(2020, 1, 1, 12, 36, 0))
+        },
+        test("ZoneId") {
+          assertRoundtrips(ZoneId.of("Pacific/Auckland"))
+        },
+        test("ZoneOffset") {
+          assertRoundtrips(ZoneOffset.ofHours(5))
+        }
+      )
     )
 
   lazy val genAst: Gen[Random with Sized, Json] =
