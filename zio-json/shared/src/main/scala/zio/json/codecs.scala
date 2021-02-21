7b18b2a8f7bb31004aff9ed1330418e9111de200
Elliot Marx <elliotrmarx@gmail.com>
2021-02-21 03:21:18 -0800

    Fix issue with codecs (#183)
    * Red
    
    * Green
    
    * Fix comment

--- zio-json/shared/src/main/scala/zio/json/codecs.scala
+++ zio-json/shared/src/main/scala/zio/json/codecs.scala
@@ -27,8 +27,28 @@ trait JsonCodec[A] extends JsonDecoder[A] with JsonEncoder[A] {
   override def xmap[B](f: A => B, g: B => A): JsonCodec[B] =
     JsonCodec(encoder.contramap(g), decoder.map(f))
 }
+
 object JsonCodec {
   def apply[A](implicit jsonCodec: JsonCodec[A]): JsonCodec[A] = jsonCodec
+
+  implicit val string: JsonCodec[String]                   = JsonCodec(JsonEncoder.string, JsonDecoder.string)
+  implicit val boolean: JsonCodec[Boolean]                 = JsonCodec(JsonEncoder.boolean, JsonDecoder.boolean)
+  implicit val char: JsonCodec[Char]                       = JsonCodec(JsonEncoder.char, JsonDecoder.char)
+  implicit val long: JsonCodec[Long]                       = JsonCodec(JsonEncoder.long, JsonDecoder.long)
+  implicit val symbol: JsonCodec[Symbol]                   = JsonCodec(JsonEncoder.symbol, JsonDecoder.symbol)
+  implicit val byte: JsonCodec[Byte]                       = JsonCodec(JsonEncoder.byte, JsonDecoder.byte)
+  implicit val short: JsonCodec[Short]                     = JsonCodec(JsonEncoder.short, JsonDecoder.short)
+  implicit val int: JsonCodec[Int]                         = JsonCodec(JsonEncoder.int, JsonDecoder.int)
+  implicit val bigInteger: JsonCodec[java.math.BigInteger] = JsonCodec(JsonEncoder.bigInteger, JsonDecoder.bigInteger)
+  implicit val double: JsonCodec[Double]                   = JsonCodec(JsonEncoder.double, JsonDecoder.double)
+  implicit val float: JsonCodec[Float]                     = JsonCodec(JsonEncoder.float, JsonDecoder.float)
+  implicit val bigDecimal: JsonCodec[java.math.BigDecimal] = JsonCodec(JsonEncoder.bigDecimal, JsonDecoder.bigDecimal)
+
+  implicit def option[A](implicit c: JsonCodec[A]): JsonCodec[Option[A]] =
+    JsonCodec(JsonEncoder.option(c.encoder), JsonDecoder.option(c.decoder))
+
+  implicit def either[A, B](implicit ac: JsonCodec[A], bc: JsonCodec[B]): JsonCodec[Either[A, B]] =
+    JsonCodec(JsonEncoder.either(ac.encoder, bc.encoder), JsonDecoder.either(ac.decoder, bc.decoder))
 
   /**
    * Derives a `JsonCodec[A]` from an encoder and a decoder.
@@ -41,14 +61,17 @@ object JsonCodec {
         e
       case other =>
         new JsonCodec[A] {
-          def encoder: JsonEncoder[A] = encoder0
+          override def encoder: JsonEncoder[A] = encoder0
 
-          def decoder: JsonDecoder[A] = decoder0
+          override def decoder: JsonDecoder[A] = decoder0
 
-          def unsafeDecode(trace: List[JsonError], in: RetractReader): A =
+          override def unsafeDecodeMissing(trace: List[JsonError]): A =
+            decoder0.unsafeDecodeMissing(trace)
+
+          override def unsafeDecode(trace: List[JsonError], in: RetractReader): A =
             decoder0.unsafeDecode(trace, in)
 
-          def unsafeEncode(a: A, indent: Option[Int], out: Write): Unit =
+          override def unsafeEncode(a: A, indent: Option[Int], out: Write): Unit =
             encoder0.unsafeEncode(a, indent, out)
         }
     }
