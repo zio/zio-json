0087ac920ad9edb02947057994b01e022bf1c63a
Ferdinand Svehla <ferdinand@fsvehla.at>
2021-03-14 19:40:23 +0100

    WithRecordingReader: decrement writing after retracting delegate

--- zio-json/shared/src/main/scala/zio/json/internal/readers.scala
+++ zio-json/shared/src/main/scala/zio/json/internal/readers.scala
@@ -198,8 +198,12 @@ private[zio] final class WithRecordingReader(in: OneCharReader, initial: Int)
   def retract(): Unit =
     if (reading == -1) {
       in match {
-        case rr: RetractReader => rr.retract()
-        case _ => reading = writing - 1
+        case rr: RetractReader =>
+          rr.retract()
+          writing -= 1 // factor in retracted delegate
+
+        case _ =>
+          reading = writing - 1
       }
     }
     else
