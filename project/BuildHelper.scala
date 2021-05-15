8e042c09c75455d2ca6f9d6ae0e2a07ec9f216ce
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-15 09:24:23 +0200

    Update scaluzzi to 0.1.18 (#191)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -205,7 +205,7 @@ object BuildHelper {
     ThisBuild / scalafixScalaBinaryVersion := CrossVersion.binaryScalaVersion(scalaVersion.value),
     ThisBuild / scalafixDependencies ++= List(
       "com.github.liancheng" %% "organize-imports" % "0.5.0",
-      "com.github.vovapolu"  %% "scaluzzi"         % "0.1.17"
+      "com.github.vovapolu"  %% "scaluzzi"         % "0.1.18"
     ),
     parallelExecution in Test := true,
     incOptions ~= (_.withLogRecompileOnMacro(false)),
