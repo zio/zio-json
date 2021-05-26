22342bdbe70765e4c8e749cd0ca4606c1f32a6b0
Scala Steward <me@scala-steward.org>
2021-05-26 22:29:05 +0200

    Update jmh-core, jmh-generator-asm, ... to 1.32

--- project/NeoJmhPlugin.scala
+++ project/NeoJmhPlugin.scala
@@ -45,7 +45,7 @@ object NeoJmhPlugin extends AutoPlugin {
   override def projectConfigurations = Seq(Jmh, JmhInternal)
 
   override def buildSettings = Seq(
-    jmhVersion := "1.31",
+    jmhVersion := "1.32",
     jmhExtrasVersion := "0.3.7"
   )
 
