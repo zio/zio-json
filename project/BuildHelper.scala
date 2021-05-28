87e616e268e6b24c68f0738517af29add38cefb2
Scala Steward <me@scala-steward.org>
2021-05-28 16:11:03 +0200

    Update scala-library, scala-reflect to 2.12.14

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -12,7 +12,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
 
   private val Scala211 = "2.11.12"
-  private val Scala212 = "2.12.13"
+  private val Scala212 = "2.12.14"
   private val Scala213 = "2.13.5"
   val DottyVersion     = "3.0.0-RC1"
 
