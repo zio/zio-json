028f31ec59a17155eba5998b26c9b941b622da33
Ferdinand Svehla <ferdinand@fsvehla.at>
2022-02-24 02:52:58 +0100

    Scala 3.1

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -139,7 +139,7 @@ object BuildHelper {
 
   def extraOptions(scalaVersion: String, optimize: Boolean) =
     CrossVersion.partialVersion(scalaVersion) match {
-      case Some((3, 0)) =>
+      case Some((3, 1)) =>
         Seq(
           "-language:implicitConversions",
           "-Xignore-scala2-macros"
