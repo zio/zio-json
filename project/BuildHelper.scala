5870e6d64adb686fe797a3cedcc765f21bf8506d
Scala Steward <scala_steward@virtuslab.com>
2024-03-03 06:29:51 +0000

    Update scala3-library, ... to 3.3.3

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -20,7 +20,7 @@ object BuildHelper {
   }
   val Scala212: String   = versions("2.12")
   val Scala213: String   = versions("2.13")
-  val ScalaDotty: String = "3.3.0"
+  val ScalaDotty: String = "3.3.3"
 
   val SilencerVersion = "1.7.13"
 
