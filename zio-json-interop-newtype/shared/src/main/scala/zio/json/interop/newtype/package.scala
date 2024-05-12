c0a865ac133307ae80104a8728a84620c6fa7cee
Grigorii Berezin <grisha.berezin.1998@mail.ru>
2024-05-12 22:00:48 +0300

    chore: try to fix for publish

--- zio-json-interop-newtype/shared/src/main/scala/zio/json/interop/newtype/package.scala
+++ zio-json-interop-newtype/shared/src/main/scala/zio/json/interop/newtype/package.scala
@@ -5,15 +5,15 @@ import io.estatico.newtype.ops._
 import zio.json.{ JsonDecoder, JsonEncoder, JsonFieldDecoder, JsonFieldEncoder }
 
 package object newtype {
-  implicit def coercibleDecoder[A: Coercible[B, *], B: JsonDecoder]: JsonDecoder[A] =
+  implicit def coercibleDecoder[B: JsonDecoder, A](implicit coerce: Coercible[B, A]): JsonDecoder[A] =
     JsonDecoder[B].map(_.coerce[A])
 
-  implicit def coercibleEncoder[A: Coercible[*, B], B: JsonEncoder]: JsonEncoder[A] =
+  implicit def coercibleEncoder[B: JsonEncoder, A](implicit coerce: Coercible[A, B]): JsonEncoder[A] =
     JsonEncoder[B].contramap(_.coerce[B])
 
-  implicit def coercibleKeyDecoder[A: Coercible[B, *], B: JsonFieldDecoder]: JsonFieldDecoder[A] =
+  implicit def coercibleKeyDecoder[B: JsonFieldDecoder, A](implicit coerce: Coercible[B, A]): JsonFieldDecoder[A] =
     JsonFieldDecoder[B].map(_.coerce[A])
 
-  implicit def coercibleKeyEncoder[A: Coercible[*, B], B: JsonFieldEncoder]: JsonFieldEncoder[A] =
+  implicit def coercibleKeyEncoder[B: JsonFieldEncoder, A](implicit coerce: Coercible[A, B]): JsonFieldEncoder[A] =
     JsonFieldEncoder[B].contramap(_.coerce[B])
 }
