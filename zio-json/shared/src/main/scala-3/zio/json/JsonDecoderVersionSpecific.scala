86170606ee6e1b1add7790499c99378d0b7b26a1
Jisoo Park <jisoo.park@doomoolmori.com>
2024-05-17 21:04:55 +0900

    Add JsonCodec derivation from encoder and decoder (#1115)
    * Add JsonCodec derivation from encoder and decoder
    * Disable JsonCodec derivaton from encoder & decoder for Scala 2.12

--- zio-json/shared/src/main/scala-3/zio/json/JsonDecoderVersionSpecific.scala
+++ zio-json/shared/src/main/scala-3/zio/json/JsonDecoderVersionSpecific.scala
@@ -1,5 +1,5 @@
 package zio.json
 
-trait JsonDecoderVersionSpecific {
+private[json] trait JsonDecoderVersionSpecific {
   inline def derived[A: deriving.Mirror.Of]: JsonDecoder[A] = DeriveJsonDecoder.gen[A]
 }
