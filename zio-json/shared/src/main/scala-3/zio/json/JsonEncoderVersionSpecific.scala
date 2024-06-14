b36bbfbed0aaff193814cb77738cb93605b4b7db
Thijs Broersen <mht.broersen@gmail.com>
2024-06-14 23:27:02 +0200

    refactor: scala 3 const enum: simplify and cleanup

--- zio-json/shared/src/main/scala-3/zio/json/JsonEncoderVersionSpecific.scala
+++ zio-json/shared/src/main/scala-3/zio/json/JsonEncoderVersionSpecific.scala
@@ -10,7 +10,4 @@ private[json] trait EncoderLowPriorityVersionSpecific {
 
   inline given unionOfStringEnumeration[T](using IsUnionOf[String, T]): JsonEncoder[T] =
     JsonEncoder.string.asInstanceOf[JsonEncoder[T]]
-
-  inline given constStringToEnum[T <: String](using IsConst[T] =:= true): JsonEncoder[T] =
-    JsonEncoder.string.narrow[T]
 }
