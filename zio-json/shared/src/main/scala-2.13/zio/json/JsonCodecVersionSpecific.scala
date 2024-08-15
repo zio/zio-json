0a555ac416f28daa67e2ee5b0eecb68b2471c553
Patrick D. Conti <contact@patrickconti.fr>
2024-08-15 06:27:33 +0200

    fix(JsonCodec): codec derivation on nested types (#1139)

--- zio-json/shared/src/main/scala-2.13/zio/json/JsonCodecVersionSpecific.scala
+++
@@ -1,7 +0,0 @@
-package zio.json
-
-private[json] trait JsonCodecVersionSpecific {
-
-  implicit def fromEncoderDecoder[A](implicit encoder: JsonEncoder[A], decoder: JsonDecoder[A]): JsonCodec[A] =
-    JsonCodec(encoder, decoder)
-}
