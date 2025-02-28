d179bf3abfb63dda2b2dd3397778b4dbb111ea41
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-02-28 11:38:59 +0100

    An example of a custom codec for union of standard types using an internal API (#1349)

--- zio-json/shared/src/test/scala-3/zio/json/CodecVersionSpecificSpec.scala
+++ zio-json/shared/src/test/scala-3/zio/json/CodecVersionSpecificSpec.scala
@@ -38,6 +38,50 @@ object CodecVersionSpecificSpec extends ZIOSpecDefault {
         case class Foo(aOrB: "A" | "B", optA: Option["A"]) derives JsonCodec
 
         assertTrue(Foo("A", Some("A")).toJson.fromJson[Foo] == Right(Foo("A", Some("A"))))
+      },
+      test("Custom codec for union of standard types using an internal API") {
+        import zio.json.internal._
+
+        type Value = Null | String | Int | Boolean
+
+        final case class MyDomain(v: Value)
+
+        object MyDomain:
+          given JsonCodec[MyDomain] = new JsonCodec[MyDomain](
+            (a: MyDomain, indent: Option[Int], out: Write) =>
+              a.v match {
+                case i: Int     => SafeNumbers.write(i, out)
+                case b: Boolean => out.write(if (b) "true" else "false")
+                case s: String  => JsonEncoder.string.unsafeEncode(s, indent, out)
+                case null       => out.write("null")
+              },
+            (trace: List[JsonError], in: RetractReader) =>
+              new MyDomain({
+                val c = in.nextNonWhitespace()
+                if (c == '"') {
+                  in.retract()
+                  Lexer.string(trace, in).toString
+                } else if (c == 't' && in.readChar() == 'r' && in.readChar() == 'u' && in.readChar() == 'e') {
+                  true
+                } else if (
+                  c == 'f' && in.readChar() == 'a' && in.readChar() == 'l' && in.readChar() == 's' && in
+                    .readChar() == 'e'
+                ) {
+                  false
+                } else if (c == 'n' && in.readChar() == 'u' && in.readChar() == 'l' && in.readChar() == 'l') {
+                  null
+                } else {
+                  in.retract()
+                  Lexer.int(trace, in)
+                }
+              })
+          )
+
+        assertTrue(
+          List(MyDomain("xxx"), MyDomain(777), MyDomain(true), MyDomain(false), MyDomain(null)).toJson
+            .fromJson[List[MyDomain]] ==
+            Right(List(MyDomain("xxx"), MyDomain(777), MyDomain(true), MyDomain(false), MyDomain(null)))
+        )
       }
     )
 }
