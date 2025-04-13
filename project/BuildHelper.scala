113967ab19ab0ea66a2c5b04e8cd6e1369237357
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-04-13 09:24:08 +0200

    Update Scala Native to 0.5.7

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -285,12 +285,12 @@ object BuildHelper {
 
   def nativeSettings = Seq(
     nativeConfig ~= { cfg =>
-      import scala.scalanative.build.{ GC, Mode }
+      import scala.scalanative.build.Mode
 
       val os = System.getProperty("os.name").toLowerCase
       // For some unknown reason, we can't run the test suites in debug mode on MacOS
       if (os.contains("mac")) cfg.withMode(Mode.releaseFast)
-      else cfg.withGC(GC.boehm) // See https://github.com/scala-native/scala-native/issues/4032
+      else cfg
     },
     scalacOptions += "-P:scalanative:genStaticForwardersForNonTopLevelObjects",
     Test / fork := false
