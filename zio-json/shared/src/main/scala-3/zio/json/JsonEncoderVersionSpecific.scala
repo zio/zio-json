a8a614ccc6728a55f9f9b3764e8aa5a1cd9e13e1
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2025-07-14 06:24:38 +0200

    Add `IArray` support (#1439) (#1440)

--- zio-json/shared/src/main/scala-3/zio/json/JsonEncoderVersionSpecific.scala
+++ zio-json/shared/src/main/scala-3/zio/json/JsonEncoderVersionSpecific.scala
@@ -7,9 +7,6 @@ import scala.collection.immutable
 import scala.compiletime.ops.any.IsConst
 
 private[json] trait JsonEncoderVersionSpecific {
-  inline def derived[A: deriving.Mirror.Of](using config: JsonCodecConfiguration): JsonEncoder[A] =
-    DeriveJsonEncoder.gen[A]
-
   implicit def arraySeq[A: JsonEncoder: scala.reflect.ClassTag]: JsonEncoder[immutable.ArraySeq[A]] =
     new JsonEncoder[immutable.ArraySeq[A]] {
       private[this] val arrayEnc = JsonEncoder.array[A]
@@ -22,6 +19,13 @@ private[json] trait JsonEncoderVersionSpecific {
       override final def toJsonAST(as: immutable.ArraySeq[A]): Either[String, Json] =
         arrayEnc.toJsonAST(as.unsafeArray.asInstanceOf[Array[A]])
     }
+
+  inline def derived[A: deriving.Mirror.Of](using config: JsonCodecConfiguration): JsonEncoder[A] =
+    DeriveJsonEncoder.gen[A]
+
+  implicit def iArray[A](implicit A: JsonEncoder[A], classTag: scala.reflect.ClassTag[A]): JsonEncoder[IArray[A]] =
+    JsonEncoder.array[A].contramap[IArray[A]](arr => IArray.genericWrapArray(arr).toArray)
+
 }
 
 private[json] trait EncoderLowPriorityVersionSpecific {
