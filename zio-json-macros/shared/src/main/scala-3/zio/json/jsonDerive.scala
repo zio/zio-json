4356c1082d4dd6304f84a58e0a092f4aa416e59f
Babigdk <Babigdk@gmail.com>
2026-04-12 23:56:24 -1100

    Fix scalafmt formatting for jsonDerive.scala in Scala 3

--- zio-json-macros/shared/src/main/scala-3/zio/json/jsonDerive.scala
+++ zio-json-macros/shared/src/main/scala-3/zio/json/jsonDerive.scala
@@ -5,9 +5,7 @@ import scala.annotation.StaticAnnotation
 /**
  * Scala 3 compatible version of @jsonDerive.
  *
- * NOTE:
- * Macro annotations are not supported in Scala 3.
- * This exists only for source compatibility.
+ * NOTE: Macro annotations are not supported in Scala 3. This exists only for source compatibility.
  *
  * Use DeriveJsonCodec.gen[A] instead.
  */
